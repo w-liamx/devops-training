ac11926684bbbcb85b991f9e280080355d1c2cb1
Williams Afiukwa <williamsafiukwa@gmail.com>
2022-08-15 10:03:42 +0000

    feat: modified build script | compress build files

--- scripts/build-client.sh
+++ scripts/build-client.sh
@@ -20,6 +20,8 @@
 
 app_dir=./apps
 client_dir="$app_dir/angular-app"
+app_build_files="./dist/app"
+buildFile="client-app.zip"
 
 ! [ -d $app_dir ] && mkdir $app_dir
 [[ -d $app_dir && -d $client_dir ]] && rm -rf $client_dir
@@ -29,6 +31,20 @@ mkdir $client_dir
 git clone https://github.com/EPAM-JS-Competency-center/shop-angular-cloudfront.git $client_dir
 cd $client_dir
 
-echo 'export NG_CLI_ANALYTICS=ci' >> $BASH_ENV
+# Turn off analytics
+export NG_CLI_ANALYTICS="false"
+
 npm i -y
-npm run build
+ng build --configuration production
+
+# Remove any existing build file
+if [ -e "$buildFile" ]; then
+	echo "Found existing build file $buildFile"
+  rm -f "$buildFile"
+  echo "Removed build file $buildFile"
+fi
+
+# Zip build
+echo "Zipping files..."
+cd $app_build_files
+zip -r ../$buildFile *
